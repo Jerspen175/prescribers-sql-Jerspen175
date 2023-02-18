1aab777996b6297a9cf96a8c3f3901e38685c2f1
Jerod Spence <spencejerod@gmail.com>
2023-02-18 11:22:32 -0600

    Question 7b

--- scripts/prescribers.sql
+++ scripts/prescribers.sql
@@ -163,10 +163,24 @@ ORDER BY total_claim_count DESC;
 -- 7. The goal of this exercise is to generate a full list of all pain management specialists in Nashville and the number of claims they had for each opioid. **Hint:** The results from all 3 parts will have 637 rows.
 
 --     a. First, create a list of all npi/drug_name combinations for pain management specialists (specialty_description = 'Pain Managment') in the city of Nashville (nppes_provider_city = 'NASHVILLE'), where the drug is an opioid (opiod_drug_flag = 'Y'). **Warning:** Double-check your query before running it. You will only need to use the prescriber and drug tables since you don't need the claims numbers yet.
-SELECT drug.drug_name, specialty_description, nppes_provider_city, opioid_drug_flag
+SELECT prescriber.npi, drug.drug_name, specialty_description, nppes_provider_city
 FROM drug
 CROSS JOIN prescriber
 WHERE nppes_provider_city = 'NASHVILLE' AND opioid_drug_flag = 'Y' AND specialty_description = 'Pain Management';
 --     b. Next, report the number of claims per drug per prescriber. Be sure to include all combinations, whether or not the prescriber had any claims. You should report the npi, the drug name, and the number of claims (total_claim_count).
-    
+SELECT prescriber.npi, drug.drug_name, total_claim_count
+FROM drug
+CROSS JOIN prescriber
+LEFT JOIN prescription
+ON prescription.drug_name = drug.drug_name AND prescriber.npi= prescription.npi
+WHERE nppes_provider_city = 'NASHVILLE' AND opioid_drug_flag = 'Y' AND specialty_description = 'Pain Management'
+ORDER BY npi, drug_name;
+
 --     c. Finally, if you have not done so already, fill in any missing values for total_claim_count with 0. Hint - Google the COALESCE function.
+SELECT prescriber.npi, drug.drug_name, total_claim_count
+FROM drug
+CROSS JOIN prescriber
+LEFT JOIN prescription
+ON prescription.drug_name = drug.drug_name AND prescriber.npi= prescription.npi
+WHERE nppes_provider_city = 'NASHVILLE' AND opioid_drug_flag = 'Y' AND specialty_description = 'Pain Management'
+ORDER BY npi, drug_name;
